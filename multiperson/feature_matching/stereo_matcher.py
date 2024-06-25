d1631d788d0ad737b2314d394c6f1e788e44564e
philipqueen <queen.philip@gmail.com>
2024-06-25 15:07:26 -0600

    working 2 camera/2 object

--- multiperson/feature_matching/stereo_matcher.py
+++ multiperson/feature_matching/stereo_matcher.py
@@ -13,7 +13,7 @@ from multiperson.geometry.epipolar_geometry import (
 from multiperson.geometry.homogenize_points import (
     homogenize_data_array,
 )
-from multiperson.utilities.display import draw_and_display_lines
+from multiperson.utilities.display import draw_lines
 
 
 class StereoMatcher:
@@ -47,25 +47,18 @@ class StereoMatcher:
 
         self.fundamental = fundamental_from_camera_pair(self.camera_a, self.camera_b)
 
-        self.current_frame = -1
-
     def match_videos(self) -> None:
         self._load_videos()
+        video_writer = self._video_writer()
 
-        while True:
-            frames = self._next_frames()
-            if frames is None:
-                break
-            frame_a, frame_b = frames
+        for i in range(self.data_cams_frame_points_xy.shape[1]):
+            swapped = False
+            frame_a, frame_b = self._next_frames()
             current_points_a = self.data_cams_frame_points_xy[
-                a_index, self.current_frame, :, :
-            ]
-            current_points_b = self.data_cams_frame_points_xy[
-                b_index, self.current_frame, :, :
-            ]
-            image_b_lines = calculate_epipolar_lines(
-                self.fundamental, current_points_a
-            )
+                a_index, i, :, :
+            ].copy()  # TODO: these are probably redundant
+            current_points_b = self.data_cams_frame_points_xy[b_index, i, :, :].copy()
+            image_b_lines = calculate_epipolar_lines(self.fundamental, current_points_a)
             image_a_lines = calculate_epipolar_lines(
                 self.fundamental.T, current_points_b
             )
@@ -74,38 +67,31 @@ class StereoMatcher:
                 current_points_a, current_points_b
             )
 
-            self.data_cams_frame_points_xy[b_index, self.current_frame, :, :] = (
-                matched_image_b_points
-            )
+            if not np.all(
+                np.isclose(
+                    self.data_cams_frame_points_xy[b_index, i, :, :],
+                    matched_image_b_points,
+                )
+            ):
+                swapped = True
+                self.data_cams_frame_points_xy[b_index, i, :, :] = (
+                    matched_image_b_points
+                )
 
             matched_image_a_lines = calculate_epipolar_lines(
                 self.fundamental.T, matched_image_b_points
             )
-            if self.current_frame % 70 == 0 and self.current_frame > 0:
-                print(
-                    f"distance to b lines before matching: {calculate_distance_to_lines(current_points_b, image_b_lines)}"
-                )
-                print(
-                    f"distance to b lines after matching: {calculate_distance_to_lines(matched_image_b_points, image_b_lines)}"
-                )
-                draw_and_display_lines(
-                    frame_a,
-                    frame_b,
-                    current_points_a,
-                    current_points_b,
-                    image_a_lines,
-                    image_b_lines,
-                )
 
-                draw_and_display_lines(
-                    frame_a,
-                    frame_b,
-                    current_points_a,
-                    matched_image_b_points,
-                    matched_image_a_lines,
-                    image_b_lines,
-                )
+            original_a = draw_lines(frame_a, image_a_lines, current_points_a)
+            original_b = draw_lines(frame_b, image_b_lines, current_points_b)
+            matched_a = draw_lines(frame_a, matched_image_a_lines, current_points_a)
+            matched_b = draw_lines(frame_b, image_b_lines, matched_image_b_points)
 
+            self.add_frames(
+                video_writer, original_a, original_b, matched_a, matched_b, swapped
+            )
+
+        video_writer.release()
 
     def _match_single_frame(
         self, image_a_points: np.ndarray, image_b_points: np.ndarray
@@ -145,20 +131,21 @@ class StereoMatcher:
         An ordering is a list of integers in the range [0, self.num_objects) mapping points to lines.
         Uses the "Hungarian algorithm" to find the optimal assignment.
         """
-        # test with frame % 70
+        # Replace NaN values with a high cost
+        large_value = 1e10
+        cost_matrix = np.nan_to_num(distances, nan=large_value)
 
-        row_ind, col_ind = linear_sum_assignment(distances)
-        # print(row_ind.tolist(), col_ind.tolist())
+        row_ind, col_ind = linear_sum_assignment(cost_matrix)
         return col_ind.tolist()
 
-    def _next_frames(self) -> Optional[Tuple[np.ndarray, np.ndarray]]:
+    def _next_frames(self) -> Tuple[np.ndarray, np.ndarray]:
         reta, image_a = self.video_a.read()
         retb, image_b = self.video_b.read()
 
         if not reta or not retb:
-            return None
-
-        self.current_frame += 1
+            raise ValueError(
+                "Could not load next frame, ensure length of videos matches length of data"
+            )
 
         return image_a, image_b
 
@@ -166,13 +153,41 @@ class StereoMatcher:
         self.video_a = self._load_video(self.index_a)
         self.video_b = self._load_video(self.index_b)
 
-        self.current_frame = -1
-
     def _load_video(self, index: int) -> cv2.VideoCapture:
         video_name = self.camera_collection.by_index(index).name
         video_path = self.synchronized_video_path / f"{video_name}.mp4"
 
         return cv2.VideoCapture(str(video_path))
+
+    def _video_writer(self) -> cv2.VideoWriter:
+        return cv2.VideoWriter(
+            str(
+                Path(__file__).parent.parent.parent
+                / f"stereo_match_{self.index_a}_{self.index_b}.mp4"
+            ),
+            cv2.VideoWriter.fourcc(*"mp4v"),
+            30,
+            (1440, 1080),
+        )
+
+    def add_frames(
+        self,
+        video_writer: cv2.VideoWriter,
+        frame_a: np.ndarray,
+        frame_b: np.ndarray,
+        frame_c: np.ndarray,
+        frame_d: np.ndarray,
+        swapped: bool,
+    ) -> None:
+        # combine 4 frames in a grid, with frames a and b on top, and frames c and d on bottom
+        top_frame = np.concatenate((frame_a, frame_b), axis=1)
+        bottom_frame = np.concatenate((frame_c, frame_d), axis=1)
+        frame = np.concatenate((top_frame, bottom_frame), axis=0)
+
+        if swapped:
+            frame = cv2.circle(frame, (frame.shape[1] // 2, frame.shape[0] // 2), 10, (0, 0, 255), -1)
+
+        video_writer.write(cv2.resize(frame, (1440, 1080)))
 
     def _validate_input_index(self, index_a: int, index_b: int) -> None:
         if not {index_a, index_b}.issubset(set(self.camera_collection.indexes)):
@@ -198,10 +213,6 @@ class StereoMatcher:
 
 
 if __name__ == "__main__":
-    from multiperson.geometry.homogenize_points import (
-        homogenize_single_frame_points,
-    )
-
     path_to_calibration_toml = Path(
         "/Users/philipqueen/freemocap_data/recording_sessions/2_brightest_points_2_cams/recording_14_30_34_gmt-6_calibration/recording_14_30_34_gmt-6_calibration_camera_calibration.toml"
     )
@@ -211,6 +222,9 @@ if __name__ == "__main__":
     video_path = Path(
         "/Users/philipqueen/freemocap_data/recording_sessions/2_brightest_points_2_cams/simple_test/synchronized_videos/"
     )
+    # video_path = Path(
+    #     "/Users/philipqueen/freemocap_data/recording_sessions/2_brightest_points_2_cams/complex_test/synchronized_videos/"
+    # )
 
     a_index = 0
     b_index = 1
@@ -223,6 +237,9 @@ if __name__ == "__main__":
     body_data_path = Path(
         "/Users/philipqueen/freemocap_data/recording_sessions/2_brightest_points_2_cams/simple_test/output_data/raw_data/brightestPoint2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy"
     )
+    # body_data_path = Path(
+    #     "/Users/philipqueen/freemocap_data/recording_sessions/2_brightest_points_2_cams/complex_test/output_data/raw_data/brightestPoint2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy"
+    # )
     body_data_cams_frame_points_xy = np.load(body_data_path)
 
     matcher = StereoMatcher(
@@ -235,17 +252,3 @@ if __name__ == "__main__":
     )
 
     matcher.match_videos()
-
-    # active_frame = 125
-    # point_a_data = homogenize_single_frame_points(body_data_cams_frame_points_xy[0, active_frame, :, :2])
-    # point_b_data = homogenize_single_frame_points(body_data_cams_frame_points_xy[1, active_frame, :, :2])
-
-    # print("Data before matching:")
-    # print(f"point b data: {point_b_data}")
-
-    # new_b_data, new_b_lines = matcher._match_single_frame(point_a_data, point_b_data)
-
-    # print("Data after matching:")
-    # print(f"point b data: {new_b_data}")
-
-    # print(f"new distance to b lines: {calculate_distance_to_lines(new_b_data, new_b_lines)}")
--- multiperson/utilities/display.py
+++ multiperson/utilities/display.py
@@ -18,15 +18,22 @@ def draw_lines(image: np.ndarray, lines: np.ndarray, points: np.ndarray):
     Lines are represent in standard form, ax + by + c = 0, where a, b, and c are the first dimension of the output array.
     The shape of lines should be the transpose of the shape of points
     """
-    rows, columns = image.shape[:2]
+    annotated_image = image.copy()
+    rows, columns = annotated_image.shape[:2]
+    np.random.seed(0)
     for rows, point in zip(lines.T, points):
         # print(f"rows: {rows}, point: {point}")
         color = tuple(np.random.randint(0, 255, 3).tolist())
-        x0, y0 = map(int, [0, -rows[2] / rows[1]])
-        x1, y1 = map(int, [columns, -(rows[2] + rows[0] * columns) / rows[1]])
-        image = cv2.line(image, (x0, y0), (x1, y1), color, 1)
-        image = cv2.circle(image, tuple(point[:2].astype(int)), 5, color, -1)
-    return image
+        try: # TODO: better way to deal with NaNs
+            x0, y0 = map(int, [0, -rows[2] / rows[1]])
+            x1, y1 = map(int, [columns, -(rows[2] + rows[0] * columns) / rows[1]])
+            annotated_image = cv2.line(annotated_image, (x0, y0), (x1, y1), color, 1)
+        except (ValueError, ZeroDivisionError):
+            pass
+        if not np.any(np.isnan(point[:2])):
+            annotated_image = cv2.circle(annotated_image, tuple(point[:2].astype(int)), 5, color, -1)
+
+    return annotated_image
 
 def draw_and_display_lines(
     image_a: np.ndarray,
